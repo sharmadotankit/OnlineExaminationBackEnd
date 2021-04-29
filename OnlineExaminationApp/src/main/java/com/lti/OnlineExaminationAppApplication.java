360bbcae16cac2d85b3a122d6b8b6f7766f081e2
sharmadotankit <sharma.ankit.kamal@gmail.com>
2021-04-29 19:38:58 +0530

    My first commit

---
+++ OnlineExaminationApp/src/main/java/com/lti/OnlineExaminationAppApplication.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExaminationAppApplication.class, args);
+	}
+
+}
